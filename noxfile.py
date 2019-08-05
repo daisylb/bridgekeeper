9b098a340f673256df6053e88b9268a1cb15fa3a
Leigh Brenecki <leigh@brenecki.id.au>
2019-08-05 14:57:20 +1000

    Run docs through Nox

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,6 @@
 import nox
 
+nox.options.sessions = 'tests'
 nox.options.reuse_existing_virtualenvs = True
 nox.options.error_on_external_run = True
 
@@ -15,3 +16,15 @@ def tests(session, django):
                 # its own virtualenv
                 env={'VIRTUAL_ENV': session.virtualenv.location})
     session.run('pytest')
+
+
+@nox.session(python='3.7')
+def docs(session):
+    session.install('poetry')
+    session.install('django')
+    session.install('sphinx==1.6.5', 'sphinx-rtd-theme==0.2.4')
+    session.run('poetry', 'install',
+                # this is necessary to prevent poetry from creating
+                # its own virtualenv
+                env={'VIRTUAL_ENV': session.virtualenv.location})
+    session.run('sphinx-build', 'docs', 'docs/_build')
